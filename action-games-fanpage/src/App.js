b196c5063ea9cb05e183366eac1b447444c08e70
Alex Minassian <alexminassianQC@gmail.com>
2023-10-06 16:29:17 -0400

    styled landing page and navbar

--- action-games-fanpage/src/App.js
+++ action-games-fanpage/src/App.js
@@ -1,7 +1,7 @@
 import GamesDisplay from './components/GamesDisplay.js';
 import './App.css';
 import Landing from './components/Landing';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PostViewer from './components/PostViewer.js';
 import Nav from './components/Nav.js';
 
@@ -10,13 +10,15 @@ function App() {
     <div>
       <h1 className='header'>Ready, Set, Fight!</h1>
       <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/singleplayer" element={ <PostViewer category={'singleplayer'}/> } />
-          <Route path="/multiplayer" element={ <PostViewer category={'multiplayer'}/> } />
-          <Route path="/gamesdisplay" element={ <GamesDisplay/> } />
-        </Routes>
+        <main className='container'>
+          <Nav />
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/singleplayer" element={<PostViewer category={'singleplayer'} />} />
+            <Route path="/multiplayer" element={<PostViewer category={'multiplayer'} />} />
+            <Route path="/gamesdisplay" element={<GamesDisplay />} />
+          </Routes>
+        </main>
       </Router>
     </div>
   );
--- action-games-fanpage/src/components/Landing.js
+++ action-games-fanpage/src/components/Landing.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 function Landing() {
     return (
-        <div className="container-fluid">
-            <p className='description'>
-                Welcome to Ready, Set, Fight! A fanpage dedicated to informing you about the best action games ranging from single-player to multiplayer!
-            </p>
+        <div>
+            <div className='container'>
+                <p className='description'>
+                    Welcome to Ready, Set, Fight! A fanpage dedicated to informing you about the best action games ranging from single-player to multiplayer!
+                </p>
+            </div>
             <div className="landing">
                 <img className="img-fluid border border-secondary rounded-5"
                     src="https://eu-images.contentstack.com/v3/assets/bltbc1876152fcd9f07/bltdc7053e28efbdeef/646b8e6706a76db286e15e1e/spider_man_2_ps5.jpg" alt="spidermen" />
