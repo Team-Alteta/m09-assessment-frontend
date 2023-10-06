61a4c5c5ba90066b03e8763e6eabfd82f9bc7786
Terrell Terry <terrell.g.terry@gmail.com>
2023-10-06 08:38:23 -0700

    Added the routes and pages for multiplayer and singleplayer

--- action-games-fanpage/src/App.js
+++ action-games-fanpage/src/App.js
@@ -3,6 +3,7 @@ import GamesDisplay from './components/GamesDisplay.js';
 import './App.css';
 import Landing from './components/Landing';
 import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import PostViewer from './components/PostViewer.js';
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
         {/* <Navbar /> */}
         <Routes>
           <Route path="/" element={<Landing />} />
+          <Route path="/singleplayer" element={ <PostViewer category={'singleplayer'}/> } />
+          <Route path="/multiplayer" element={ <PostViewer category={'multiplayer'}/> } />
+          <Route path="/gamesdisplay" element={ <GamesDisplay/> } />
         </Routes>
       </Router>
     </div>
